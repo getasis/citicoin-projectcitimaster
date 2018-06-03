714e761f8f2ad0058c80537cb438ccff8f65bf0b
getasis <getasis91@gmail.com>
2018-06-03 17:07:21 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1090,7 +1090,7 @@ int64 static GetBlockValue(int nHeight, int64 nFees)
     int64 nSubsidy = 50 * COIN;
 
     // Subsidy is cut in half every 840000 blocks, which will occur approximately every 4 years
-    nSubsidy >>= (nHeight / 500000); // citicoin: 500k blocks in ~4 years
+    nSubsidy >>= (nHeight / 50000); // citicoin: 500k blocks in ~4 years
 
     return nSubsidy + nFees;
 }
